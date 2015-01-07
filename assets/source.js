b8151b3ceafd1cf722fcccdce95c61200dae3e72
Victor Aprea <victor.aprea@wickeddevice.com>
2015-01-07 16:53:37 -0500

    adding web server source files

---
+++ assets/source.js
@@ -0,0 +1,34 @@
+function ledStatus (arg) {
+    function responseHandler(data, textStatus, jqXHR){
+      if(data.status == "ok"){
+        $("#response").text(data.led);
+      }
+      else{
+        $("#response").text("Error: " + data.path);
+      }
+
+    }
+    //all the stuff about getting the status back goes here.
+    switch(arg){
+      case "on":
+        $.post("/led/on", responseHandler);
+        break;
+      case "off":
+        $.post("/led/off", responseHandler);
+        break;
+      case "toggle":
+        $.post("/led/toggle", responseHandler);
+        break;
+      default:
+
+    }
+}
+
+$(document).ready(function(){
+    //This handles all buttons. data-func value MUST be the same as function name
+    $(".actionbutton").click(function(){
+        var func = $(this).data("func");
+        var args = $(this).data("args");
+        window[func](args);
+    });
+});
