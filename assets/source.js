5057785e6fb1706ee3d050493e0f7cb58852f7cb
Dirk <dirk.swart@wickeddevice.com>
2015-01-08 11:09:57 -0500

    Changed based on SOA approach

--- assets/source.js
+++ assets/source.js
@@ -1,13 +1,12 @@
 function ledStatus (arg) {
     function responseHandler(data, textStatus, jqXHR){
       if(data.status == "ok"){
-        $("#response").text("LED is: " + data.led);
+        $("#response").text("Light is: " + data.led);
       }
       else{
         $("#response").text("Error: " + data.path);
       }
     }
-
     //all the stuff about getting the status back goes here.
     switch(arg){
       case "on":
@@ -20,34 +19,6 @@ function ledStatus (arg) {
         $.post("/led/toggle", responseHandler);
         break;
       default:
-    }
-}
-
-// One option is to use a form field to set an angle, and the JSON
-// can reply with the setting the Servo is currently in.
-// Note data.position string.
-function servoStatus (arg) {
-    function responseHandler(data, textStatus, jqXHR){
-        if(data.status == "ok"){
-            $("#response").text("Servo is in position: " + data.position);
-        }
-        else{
-            $("#response").text("Error: " + data.path);
-        }
-    }
-
-    //all the stuff about getting the status back goes here.
-    switch(arg){
-        case "on":
-            $.post("/led/on", responseHandler);
-            break;
-        case "off":
-            $.post("/led/off", responseHandler);
-            break;
-        case "toggle":
-            $.post("/led/toggle", responseHandler);
-            break;
-        default:
     }
 }
 
